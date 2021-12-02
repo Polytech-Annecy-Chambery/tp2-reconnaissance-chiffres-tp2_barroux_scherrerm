b53195d588f395fdf609682b8b9e6362ff2285e5
hugobarroux <95025177+hugobarroux@users.noreply.github.com>
2021-12-02 11:16:53 +0100

    similitude

--- src/image.py
+++ src/image.py
@@ -108,5 +108,12 @@ class Image:
     # Methode de mesure de similitude entre l'image self et un modele im
     #==============================================================================
     def similitude(self, im):
-        pass
+        c = 0
+        for i in range(self.H):
+          for j in range (self.W):
+              if self.pixels[i,j]==im.pixels[i,j]:
+                  c+=1
+        res= c/(self.H*self.W)
+        return res
+              
 
